9d53f09fe4dc1c75715c95448915497292894dbd
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-06 15:04:24 -0300

    test(user): adiciona specs de validação de nome

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should allow_value('user@example.com').for(:email) }
     it { should_not allow_value('invalid_email').for(:email) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(2) }
+    it { should validate_length_of(:name).is_at_most(100) }
   end
 end
