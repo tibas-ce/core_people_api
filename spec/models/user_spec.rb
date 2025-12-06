3d4e854c677bb31ca74f37fc54760f6ab125521c
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-06 14:55:28 -0300

    test(user): adiciona specs de validação de email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,12 @@ RSpec.describe User, type: :model do
       )
       expect(user).to be_valid
     end
+
+    # Testes com a gem Shoulda Matchers que fornece “matchers” (testes prontos) para o RSpec, permitindo testar:
+    # Validações; Associações; Delegações; Estruturas de database; Permissões (em alguns casos)
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value('user@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
   end
 end
