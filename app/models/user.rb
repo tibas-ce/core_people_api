8727a64fed369f719c409231ed61c3c0320c70fa
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-06 15:15:39 -0300

    feat(user): adiciona validações de email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # Normalizações
+  before_save { self.email = email.downcase.strip }
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: {
+              with: URI::Mailto::EMAIL_REGEXP,
+              message: 'Deve ser um email válido'
+            }
 end
