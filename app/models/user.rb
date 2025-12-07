b67d9c768a2072ce9494410f45af962e7a1cf2f9
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-07 08:29:09 -0300

    fix(user): criando função de normalização de email e ajustando callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # Normalizações
-  before_save { self.email = email.downcase.strip }
+  before_save :normalize_email
 
   validates :email,
             presence: true,
@@ -22,6 +22,10 @@ class User < ApplicationRecord
             if: :password_required?
 
   private
+
+  def normalize_email
+    self.email = email.to_s.downcase.strip
+  end
 
   def password_required?
     password_digest.blank? || password.present?
