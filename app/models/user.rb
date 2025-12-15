641f2f24101350cbabceab0f515edc88d199a551
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-15 10:45:32 -0300

    feat(role): atualização do Model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
+  # Autenticação
   has_secure_password
 
-  # Normalizações
-  before_save :normalize_email
+  # Associações
+  has_one :role, dependent: :destroy
 
+  # Callsbacks
+  before_save :normalize_email
+  after_create :assign_default_role
+
+  # Validações
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             format: {
               with: URI::MailTo::EMAIL_REGEXP,
-              message: 'Deve ser um email válido'
+              message: "Deve ser um email válido"
             }
 
   validates :name,
@@ -21,13 +27,27 @@ class User < ApplicationRecord
             length: { minimum: 6 },
             if: :password_required?
 
+  # Métodos de Role
+  delegate :admin?, :hr?, :manager?, :employee?, to: :role, allow_nil: true
+
+  def role_name
+    role&.name
+  end
+
   private
 
+  # Normaliza email antes de salvar
   def normalize_email
     self.email = email.to_s.downcase.strip
   end
 
+  # Exige senha apenas quando necessário
   def password_required?
     password_digest.blank? || password.present?
+  end
+
+  # Garante que todo usuário tenha um role
+  def assign_default_role
+    create_role(name: Role::EMPLOYEE) unless role
   end
 end
