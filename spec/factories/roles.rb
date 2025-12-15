67442bc44360afe6bf757e352c278458c44cabb7
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-15 10:54:04 -0300

    feat(role): cria Factory de Role

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,6 +1,21 @@
 FactoryBot.define do
   factory :role do
-    user { nil }
-    name { "MyString" }
+    # Cada role pertence a um usuário
+    association :user
+
+    # Role padrão
+    name { Role::EMPLOYEE }
+
+    trait :admin do
+      name { Role::ADMIN }
+    end
+
+    trait :hr do
+      name { Role::HR }
+    end
+
+    trait :manager do
+      name { Role::MANAGER }
+    end
   end
 end
