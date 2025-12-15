bd6f7866349fbfe8e83eab05794077d6ec924c3f
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-12-15 09:57:13 -0300

    feat(role): editando e rodando a migrantion Role

--- db/migrate/20251215123742_create_roles.rb
+++ db/migrate/20251215123742_create_roles.rb
@@ -1,10 +1,12 @@
 class CreateRoles < ActiveRecord::Migration[8.0]
   def change
     create_table :roles do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :name
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
+      t.string :name, null: false
 
       t.timestamps
     end
+
+    add_index :roles, :name
   end
 end
